84d99e31dea99448e3f90ea2df7bec56d5fcc93e
Shantanu Padhye <shantanu.padhye@myob.com>
2021-03-02 22:16:40 +1300

    clean up the code a little

--- divisorFuncs.js
+++ divisorFuncs.js
@@ -1,24 +1,21 @@
 const _ = require('lodash');
 
-const range = (rangeInfo) => { return [...Array(rangeInfo.upper - 1).keys()].map(i => i + rangeInfo.lower + 1) };
+const range = (rangeInfo) => [...Array(rangeInfo.upper - 1).keys()].map(i => i + rangeInfo.lower + 1);
   
 const calculateResult = (number, divisorData) => {
-    var divisors = divisorData.outputDetails;
-    var result = _.chain(divisors)
+    var result = _.chain(divisorData.outputDetails)
                     .map((divisor) => (number % divisor.divisor) ? '' : divisor.output)
                     .join('')
                     .value();
     return { 'number':number, 'result': result };
 }
 
-const getResult = (rangeInfo, divisorInfo) => {
-    const numbers = range(rangeInfo);
-    return  _.chain(numbers)
-                .map((number) => calculateResult(number, divisorInfo))
-                .map((divided) => `${divided.number}: ${divided.result}`)
-                .join('<br/>')
-                .value();
-}
+const getResult = (rangeInfo, divisorInfo) => _.chain(range(rangeInfo))
+                                                .map((number) => calculateResult(number, divisorInfo))
+                                                .map((divided) => `${divided.number}: ${divided.result}`)
+                                                .join('<br/>')
+                                                .value();
+
 
 module.exports = {
     range: range,
